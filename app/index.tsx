8d72b2265b7eec32f51ccd79552bd593a1e777e9
Xyler7 <kutaydemir4539@gmail.com>
2025-07-25 17:21:48 +0300

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,14 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/login"
+        style={{
+          width: 100,
+          height:20,
+          backgroundColor: "coral",
+          borderRadius: 8,
+          textAlign:"center"}}> Login Page
+        </Link>
     </View>
   );
 }
